2b616113b555100ecd407feae2ad0301d04581ea
pm201817 <36443044+pm201817@users.noreply.github.com>
2018-03-05 16:51:58 +0530

    Another new test case number 4 added

---
+++ DemoEclipse/src/DemoTest/Test4.java
@@ -0,0 +1,27 @@
+package DemoTest;
+
+import java.util.Scanner;
+
+public class Test4 {
+
+	public static void main(String[] args) {
+		
+		String a[] = new String[5];		
+		Scanner sc=new Scanner(System.in);
+				
+		for (int i = 0; i < a.length; i++)
+		{
+			System.out.println("Enter names of students ");
+						a[i]= sc.next();
+		}
+		
+		System.out.println("\nList of Arrary entered by User is ");
+		for (int i = 0; i < a.length; i++)
+		{
+			System.out.print(a[i] + "\n");
+		}
+		sc.close();
+
+	}
+
+}
